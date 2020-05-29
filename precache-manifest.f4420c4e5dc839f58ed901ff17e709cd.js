399a31a041011b64dc541dfe586de598b285817a
RanieryMendes <mendrc18@wfu.edu>
2020-05-29 14:15:20 -0300

    Updates

---
+++ precache-manifest.f4420c4e5dc839f58ed901ff17e709cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93423d5b89ba537aca953fd68f5e9be7",
+    "url": "/Project_React-2/index.html"
+  },
+  {
+    "revision": "7cf0b3d1435ce8fe4235",
+    "url": "/Project_React-2/static/css/main.74bc9e29.chunk.css"
+  },
+  {
+    "revision": "382239009c5495fe3183",
+    "url": "/Project_React-2/static/js/2.953d19bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Project_React-2/static/js/2.953d19bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cf0b3d1435ce8fe4235",
+    "url": "/Project_React-2/static/js/main.dc6a3299.chunk.js"
+  },
+  {
+    "revision": "64f52daf5ee95d5f3a8d",
+    "url": "/Project_React-2/static/js/runtime-main.89a909e1.js"
+  }
+]);
